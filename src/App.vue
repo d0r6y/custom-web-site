36801ce992a1f7968aa16bffebc026635fc7075e
d0r6y <solmontea98@gmail.com>
2021-01-04 04:21:56 +0900

    error handling

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,96 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = !drawer"/>
-      <v-toolbar-title>Page Title</v-toolbar-title>
-      <v-spacer/>
-      <v-btn icon to="/about">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon to="/">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="site.title"></site-title>
+      <v-spacer />
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
+      <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
-    <v-footer app color="primary" dark absolute>
-      <v-spacer></v-spacer>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from "@/views/site/title";
+import SiteFooter from "@/views/site/footer";
+import SiteMenu from "@/views/site/menu";
+
 export default {
-  name: 'App',
-  data () {
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: "App",
+  data() {
     return {
-      drawer: false
-    }
-  }
-}
+      drawer: false,
+      site: {
+        menu: [
+          {
+            title: "home",
+            icon: "mdi-home",
+            subItems: [
+              {
+                title: "Dashboard",
+                to: "/",
+              },
+              {
+                title: "About",
+                to: "/about",
+              },
+            ],
+          },
+          {
+            title: "about",
+            active: true,
+            icon: "mdi-account",
+            subItems: [
+              {
+                title: "xxx",
+                to: "/xxx",
+              },
+            ],
+          },
+        ],
+        title: "나의 타이틀입니다",
+        footer: "푸터입니다",
+      },
+    };
+  },
+  created() {
+    this.subscribe();
+  },
+  methods: {
+    subscribe() {
+      this.$firebase
+        .database()
+        .ref()
+        .child("site")
+        .on(
+          "value",
+          (sn) => {
+            const v = sn.val();
+            if (!v) {
+              // 값이 없을떄 -> 그냥 현재 값 저장
+              this.$firebase.database().ref().child("site").set(this.site);
+              return;
+            }
+            this.site = v;
+          },
+          (e) => {
+            console.log(e.message);
+          }
+        );
+    },
+  },
+};
 </script>
